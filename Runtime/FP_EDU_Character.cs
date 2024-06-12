f22f217396c15f5a3272ed994691c75719e3241a
John Shull <the.john.shull@gmail.com>
2024-06-11 21:41:44 -0400

    Cleaning up Requirements

--- Runtime/FP_EDU_Character.cs
+++ Runtime/FP_EDU_Character.cs
@@ -5,10 +5,10 @@ namespace FuzzPhyte.Utility.EDU
 {
     [Serializable]
     [CreateAssetMenu(fileName = "Character", menuName = "FuzzPhyte/Utility/EDU/Character", order = 3)]
-    public class FP_Character : FP_EDU_Data
+    public class FP_EDU_Character : FP_EDU_Data
     {
         public string CharacterName;
-        public CC_Gender CharacterGender;
+        public FP_Gender CharacterGender;
         public FP_Theme CharacterTheme;
     }
 }
--- Runtime/FP_EDU_Data.cs
+++ Runtime/FP_EDU_Data.cs
@@ -3,22 +3,7 @@ using UnityEngine;
 
 namespace FuzzPhyte.Utility.EDU
 {
-    /// <summary>
-    /// this is used mainly for our characters so we can correctly match different services later as needed
-    /// for things like TTS and other visual updates as the game ages
-    /// this list is from "https://www.csusm.edu/ipa/surveys/inclusive-language-guidelines.html"
-    /// </summary>
-    [Serializable]
-    [SerializeField]
-    public enum CC_Gender
-    {
-        Woman,
-        Man,
-        Transgender,
-        NonBinary_NonConforming,
-        Robot,
-        Cyborg
-    }
+    
     /// <summary>
     /// Base class for all things tied to Data for EDU and Scriptable Objects
     /// </summary>
